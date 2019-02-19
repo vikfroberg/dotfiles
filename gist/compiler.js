d5e403108fc2a9ab0b120589b99440572f8bdc3f
Viktor Froberg <vikfroberg@gmail.com>
2019-02-19 14:46:04 +0100

    haha

--- gist/compiler.js
+++ gist/compiler.js
@@ -1,31 +1,65 @@
+// Result
+
+const Result = {}
+
+Result.Ok = x => ({
+  _type: "Result.Ok",
+  toString: () => `Result.Ok (${x.toString()})`,
+  andThen: fn => fn(x),
+})
+
+Result.Error = x => ({
+  _type: "Result.Error",
+  toString: () => `Result.Error (${x.toString()})`,
+  andThen: fn => Result.Error(x),
+})
+
+// Parser Combinator
+
+const Parser = (result, context) => ({
+  _type: "Parser",
+  toString: () => `Parser (${result.toString()}, ${context.toString()})`,
+})
+
+const int = context =>
+  context[0].match(/\d/)
+    ? Parser(Result.Ok(context[0]), context.slice(1))
+    : Parser(Result.Error(), context)
+
+
+const literal = context =>
+  context[0].match(/[a-z]/)
+    ? Parser(Result.Ok(context[0]), context.slice(1))
+    : Parser(Result.Error(), context)
+
+const space = context => {
+  return context[0] === " "
+    ? Parser(Result.Ok(context[0]), context.slice(1))
+    : Parser(Result.Error(), context)
+}
+
+const many = fn => input => {
+  let nextInput
+  let currentInput = input
+  while (nextInput = fn(currentInput)) {
+    currentInput = nextInput
+  }
+  return currentInput
+}
+
+const and = (f1, f2) => context => {
+  return ""
+  // return f1(context).andThen(x => f2
+}
+
+const seq = fs => input => fs.reduce((acc, fn) => acc ? fn(acc) : acc, input)
+
+// Compiler
+
+const parse = and(literal, literal) //seq([many(literal), space, int, space, int])
+
 console.log(
   "program",
-  parse("id x = x\nid2 x = x")
+  parse("add 1 1").toString()
 )
 
-function parse(str) {
-  return {
-    type: "Program",
-    module: "Main",
-    definitions: many(definition, str),
-  }
-}
-
-function many(fn, str) {
-  return definition(str, [])
-}
-
-function definition(str) {
-  const fun = match([
-    identifier,
-    zeroOrMore(match(space, identifier)),
-    equal,
-    oneOrMore(whitespace),
-  ], ([ id, args, _, _]) => ({ id, args }))
-
-  const expr = match([ identifier ], ([ id ]) => ({ type: "Expression", val: { type: "Identifier", value: id } }))
-
-  const def = match([ fun, expr ], ([ fun, expr ]) => ({ type: "Definition", name: fun.id, args: fun.args, expr: expr }))
-
-  return [ def, ...definition(str) ]
-}
--- gist/ivar.md
+++ gist/ivar.md
@@ -1,5 +1,11 @@
 # Ivar
 
+## To-do's
+* Sticky header
+* Pilar för att indikera flöde
+* Linjer för att indikera heirarki
+* Dynamisk area of concern
+
+
 * Onödigt komplicerat?
 * Ui för mobil, enklare med bara select istället för drag n drop?
-*
