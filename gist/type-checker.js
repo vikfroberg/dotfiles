29deb7947b13aa8a5103b7e3b2fb88dac72b6b19
Viktor Froberg <vikfroberg@gmail.com>
2019-03-13 13:45:14 +0100

    pilla med lang

--- gist/type-checker.js
+++ gist/type-checker.js
@@ -1,74 +1,119 @@
-const Type = {}
+// Helpers
 
-Type.Named = (name) => ({
-  nodeType: "Named",
-  name: name,
-})
-
-Type.Var = (name) => ({
-  nodeType: "Var",
-  name: name,
-})
-
-Type.Function = (...types) => {
-  return types.reduceRight((to, from) => ({
-    nodeType: "Function",
-    from: from,
-    to: to
-  }));
+function isArray(a) {
+  return Array.isArray(a)
 }
 
-const Expression = {}
+function isChar(s) {
+  return typeof s === "string" && s.length === 1
+}
 
-Expression.Function = (param, body) => ({
-  nodeType: "Function",
-  param: param,
-  body: body,
-})
+function isString(s) {
+  return typeof s === "string" && s.length > 1
+}
 
-Expression.Call = (func, arg) => ({
-  nodeType: "Call",
-  func: func,
-  arg: arg,
-})
+function isSymbol(s) {
+  return typeof s === "string" && s.startsWith("$")
+}
 
-Expression.Var = (name) => ({
-  nodeType: "Var",
-  name: name,
-})
+function isInt(n){
+  return Number(n) === n && n % 1 === 0;
+}
 
-Expression.Int = (value) => ({
-  nodeType: "Int",
-  value: value,
-})
+function isFloat(n){
+  return Number(n) === n && n % 1 !== 0;
+}
 
-function infer(env, e) {
-  switch (e.nodeType) {
-    case "Int":
-      return Type.Named("Int");
-    case "Var":
-      if (env[e.name]) {
-        return env[e.name];
+function astToString(s) {
+  return JSON.stringify(s, null, 2)
+}
+
+
+
+// Compiler
+
+const run = sexpr => {
+  function go(expr) {
+    if (isArray(expr)) {
+      if (expr[0] === "defun") {
+        const [ _, name, args, body ] = expr
+        return {
+          type: "FUNCTION",
+          name,
+          args,
+          body: go(body),
+        }
       } else {
-        throw `Unbound var ${env.name}`;
+        const [symbol, ...args] = expr
+        return args.reduce(
+          (func, arg) => ({ type: "CALL", func, arg: go(arg) }),
+          { type: "SYMBOL", val: symbol }
+        )
       }
-    case "Call":
-      throw `Unimplemented ${e.nodeType}`;
-    default:
-      throw `Unimplemented ${e.nodeType}`;
+    } else if (isSymbol(expr)) {
+      return { type: "SYMBOL", val: expr.slice(1) }
+    } else if (isString(expr)) {
+      return { type: "STRING", val: expr }
+    } else if (isChar(expr)) {
+      return { type: "CHAR", val: expr }
+    } else if (isInt(expr)) {
+      return { type: "INT", val: expr }
+    } else if (isFloat(expr)) {
+      return { type: "FLOAT", val: expr }
+    } else {
+      throw "Unknown type"
+    }
   }
+
+  return sexpr.map(go)
 }
 
-console.log(
-  infer(
-    {
-      "True": Type.Named("Bool"),
-      "False": Type.Named("Bool"),
-      "Int.inc": Type.Function(
-        Type.Named("Int"),
-        Type.Named("Int"),
-      ),
-    },
-    Expression.Call("Int.inc", Expression.Int(1)),
-  )
-)
+const astToJs = sast => {
+  function go(ast) {
+    switch (ast.type) {
+      case "FUNCTION": {
+        return `function ${ast.name}(${ast.args.join(",")}){return ${go(ast.body)}}`
+      }
+      case "CALL": {
+        return `${go(ast.func)}(${go(ast.arg)})`
+      }
+      case "STRING":
+      case "CHAR": {
+        return `"${ast.val}"`
+      }
+      case "SYMBOL":
+      case "INT":
+      case "FLOAT": {
+        return ast.val.toString()
+      }
+      default: {
+        throw ("Unknown type " + ast.type)
+      }
+    }
+  }
+
+  const env = [
+    `function add(x) { return function add_(y) { return x + y } }`,
+    `function sub(x) { return function sub_(y) { return x - y } }`,
+    `function isArray(a) { return Array.isArray(a) }`,
+    `function isChar(s) { return typeof s === "string" && s.length === 1 }`,
+    `function isString(s) { return typeof s === "string" && s.length > 1 }`,
+    `function isInt(n){ return Number(n) === n && n % 1 === 0; }`,
+    `function isFloat(n){ return Number(n) === n && n % 1 !== 0; }`,
+  ]
+  const gen = sast.map(go).join("\n")
+  return [env.join("\n"), "", gen].join("\n")
+}
+
+const app = run([
+  [ "defun", "add2", [ "x", "y" ], [ "add", "$x", "$y" ] ],
+  [ "defun", "increment", [ "x" ], [ "add", "$x", 1 ] ],
+  [ "add2", 1, 1 ],
+  [ "increment", 1 ]
+])
+
+console.log(astToString(app))
+
+console.log(astToJs(app))
+
+// console.log(eval(astToJs(app)))
