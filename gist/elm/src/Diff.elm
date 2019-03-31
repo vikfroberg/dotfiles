bef1b3a1895de346cc08c305525bda460019c08b
Viktor Froberg <vikfroberg@gmail.com>
2019-03-31 22:11:53 +0200

    m

--- gist/elm/src/Diff.elm
+++ gist/elm/src/Diff.elm
@@ -3,149 +3,103 @@ module Diff exposing (..)
 import Set
 import Dict
 import Array
+import Html
+import List.Extra as List
 
-tupleFold f ( a, b ) =
-    f a b
+listMax xs ys =
+    if List.length xs > List.length ys then
+        xs
+    else
+        ys
 
-
-arrayFind x xs =
-    xs
-        |> Array.toIndexedList
-        |> List.filterMap (\( index, y ) ->
-            if x == y then
-                Just index
-            else
-                Nothing
-        )
-        |> List.head
-
-listFind xs x =
-    xs
-        |> List.filterMap (\y ->
-            if x == y then
-                Just x
-            else
-                Nothing
-        )
-        |> List.head
-
-
-type Change comparable a
-    = Replaced a Int
-    | Removed Int
-    | Added a
-    | Moved Int Int
-
-
-toWeight change =
-    case change of
-        Replaced _ _ ->
-            0
-
-        Removed _ ->
-            1
-
-        Added _ ->
-            2
-
-        Moved _ _ ->
-            3
-
-
-fold : (Change comparable a -> b -> b) -> b -> List (Change comparable a) -> b
-fold f b xs =
-    xs
-        |> List.sortBy toWeight
-        |> List.foldl f b
-
-
-diff leftList rightList =
+listLcs xs ys j k =
     let
-        leftArray =
-            leftList
-                |> List.map Tuple.first
-                |> Array.fromList
-
-        rightArray =
-            rightList
-                |> List.map Tuple.first
-                |> Array.fromList
-
-        leftDict =
-            Dict.fromList leftList
-
-        rightDict =
-            Dict.fromList rightList
-
-        removed =
-            Dict.diff leftDict rightDict
-                |> Dict.toList
-                |> List.filterMap (\( key, val ) ->
-                    leftArray
-                        |> arrayFind key
-                        |> Maybe.map Removed
-                )
-
-        added =
-            Dict.diff rightDict leftDict
-                |> Dict.toList
-                |> List.map (Tuple.second >> Added)
-
-        replaced =
-            rightDict
-                |> Dict.toList
-                |> List.filterMap (\( key, rightValue ) ->
-                    leftDict
-                        |> Dict.get key
-                        |> Maybe.andThen (\leftValue ->
-                            if leftValue == rightValue then
-                                Nothing
-                            else
-                                leftArray
-                                    |> arrayFind key
-                                    |> Maybe.map (Replaced rightValue)
-                        )
-                )
-
-        moved =
-            List.append
-                (Dict.intersect leftDict rightDict |> Dict.toList)
-                (Dict.diff rightDict leftDict |> Dict.toList)
-                |> Array.fromList >> Array.toIndexedList
-                |> List.filterMap (\( index, ( key, val ) ) ->
-                    rightArray
-                        |> arrayFind key
-                        |> Maybe.andThen (\rightIndex ->
-                            if index == rightIndex then
-                                Nothing
-                            else
-                                if index > rightIndex then
-                                    Just (rightIndex, index)
-                                else
-                                    Just (index, rightIndex)
-                        )
-                )
-                |> Set.fromList
-                |> Set.toList
-                |> List.map (tupleFold Moved)
+        xs_ = List.tail xs |> Maybe.withDefault []
+        ys_ = List.tail ys |> Maybe.withDefault []
     in
-    replaced ++ removed ++ added ++ moved
+        if List.length xs == 0 || List.length ys == 0 then
+            []
+        else
+            Maybe.map2
+                (\x y ->
+                    if x == y then
+                        [ ( j, k ) ] ++ listLcs xs_ ys_ (j + 1) (k + 1)
+                    else
+                        listMax
+                            (listLcs xs ys_ j (k + 1))
+                            (listLcs xs_ ys (j + 1) k)
+                )
+                (List.head xs)
+                (List.head ys)
+                |> Maybe.withDefault []
 
+diff xs ys =
+    let
+        _ = Debug.log "diff" ( xs, ys )
 
-left =
-        [ ( "1", "Viktor" )
-        , ( "2", "Sanna" )
-        , ( "3", "Simon" )
-        ]
+        lcs =
+            listLcs xs ys 0 0
 
+        free =
+            lcs
+                |> List.map Tuple.second
+                |> (\zs ->
+                    Set.diff
+                        (List.range 0 (List.length ys - 1) |> Set.fromList)
+                        (zs |> Set.fromList)
+                        |> Set.toList
+                )
 
-right =
-        [ ( "1", "Viktor" )
-        , ( "2", "Sanna" )
-        , ( "4", "Alex" )
-        , ( "3", "Robin" )
-        ]
+        inserts =
+            free
+                |> List.filterMap (\to ->
+                    ys
+                        |> Array.fromList
+                        |> Array.get to
+                        |> Maybe.map (\y -> ( y, to ))
+                )
 
+        updates =
+            lcs
+                |> List.filterMap (\( from, to ) ->
+                    ys
+                        |> Array.fromList
+                        |> Array.get to
+                        |> Maybe.map (\y -> ( y, from ))
+                )
 
-changes =
-    diff left right
+        removes =
+            lcs
+                |> List.map Tuple.first
+                |> Set.fromList
+                |> (\zs ->
+                    Set.diff
+                        (List.range 0 (List.length xs - 1) |> Set.fromList)
+                        zs
+                )
+                |> Set.toList
+                -- |> List.filter (\i -> ys |> Array.fromList |> Array.get i |> Maybe.map (always False) |> Maybe.withDefault True)
+
+    in
+    Debug.log
+        "diff"
+        ( updates
+        , removes
+        , inserts
+        )
+
+test =
+    [ diff [ "a" ] [ "a", "b" ] -- add to end
+    , diff [ "a" ] [ "b", "a" ] -- add to beginning
+    , diff [ "a", "b" ] [ "a" ] -- remove end
+    , diff [ "a", "b" ] [ "b" ] -- remove beginning
+    , diff [ "a", "b" ] [ "b", "a" ] -- swap // works but should reuse A instead of insert/remove
+    , diff [ "a", "b" ] [ "a", "c", "b" ] -- add in middle
+    , diff [ "a", "b" ] [ "a", "c", "b", "d" ] -- add in and end
+    ]
+
+main =
+    let
+        _ = test
+    in
+    Html.text "hej"
