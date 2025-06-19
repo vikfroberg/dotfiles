420b7d1dc60ff0aa7558664b426505977899caa8
Viktor Fröberg <vikfroberg@gmail.com>
2025-06-19 20:36:14 +0200

    Fix install script, soon new computer

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ echo "Bootstrapping..."
 
 ~/Developer/dotfiles/install/brew.sh
 ~/Developer/dotfiles/install/symlink.sh
-~/Developer/dotfiles/install/base16.sh
 
 cat ~/Developer/dotfiles/install/enjoy.txt
